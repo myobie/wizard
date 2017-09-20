a99aa6195d5bd42a0855ac54864cc0de0ac9c03d
Nathan Herald <me@nathanherald.com>
2017-09-20 22:00:51 +0200

    I keep forgetting it's inserted_at

--- lib/wizard/feeds.ex
+++ lib/wizard/feeds.ex
@@ -20,7 +20,7 @@ defmodule Wizard.Feeds do
   def all_events(feed_id) do
     events = from(e in Event,
                   where: e.feed_id == ^feed_id,
-                  order_by: [desc: e.created_at],
+                  order_by: [desc: e.inserted_at],
                   limit: 10)
              |> Repo.all()
 
